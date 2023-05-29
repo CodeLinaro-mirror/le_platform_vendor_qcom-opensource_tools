6a1bde705ef9d537492f656ee9bf65012202b067
Pawan Chilka <quic_pchilka@quicinc.com>
2023-05-29 11:31:44 +0530

    Updating  BSD Clause license....
    Change-Id: I82c0fc1f2dbb6cf8107c7945ee162929fb24fa71

--- linux-ramdump-parser-v2/tlbdumplib.py
+++ linux-ramdump-parser-v2/tlbdumplib.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
-# Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -27,6 +26,11 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Changes from Qualcomm Innovation Center are provided under the following license:
+# Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
 
 from __future__ import print_function
 import struct
