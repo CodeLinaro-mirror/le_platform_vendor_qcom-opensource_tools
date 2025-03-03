70e544194d7e9bc5ebc913c699a26f52c54e4bae
Zhiqing Xiong <quic_zhiqxion@quicinc.com>
2025-03-03 15:01:09 +0800

    lrdp_v2: correct copyright
    Change-Id: I4c8369ada90b25f9a5917d7bfde49a40dc80b88b
    (cherry picked from commit 8e6ec67c1fb9a2bc8d109a51dbf956cb661e9463)

--- linux-ramdump-parser-v2/parsers/pstore.py
+++ linux-ramdump-parser-v2/parsers/pstore.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2018, 2020-2021, 2025 The Linux Foundation. All rights reserved.
+# Copyright (c) 2018, 2020-2021 The Linux Foundation. All rights reserved.
+# Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
